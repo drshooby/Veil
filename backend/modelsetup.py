9b6072b3a697a8f50ac15483b22646c825cc8544
David Shubov <116600844+drshooby@users.noreply.github.com>
2024-11-09 14:59:51 -0800

    add: updating modelsetup to work with flask

--- backend/modelsetup.py
+++ backend/modelsetup.py
@@ -1,7 +1,10 @@
+import os.path
+
 from facenet_pytorch import MTCNN
 import cv2
 import concurrent.futures
 import torch
+from flask import current_app
 
 
 class ModelSetup:
@@ -9,7 +12,8 @@ class ModelSetup:
     def __init__(self):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = MTCNN(keep_all=True, device=self.device)
-        self.output_path = "final_output_veil.mp4"
+        self.input_filename = "input_file"
+        self.output_path = os.path.join(current_app.config["PROCESSED_FOLDER"], f"{self.input_filename}_veiled.mp4")
 
     def model_detect(self, image) -> list:
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -26,20 +30,10 @@ class ModelSetup:
     def blur(self, locations, image):
         x1, y1, x2, y2 = map(int, locations['box'])
         roi = image[y1:y2, x1:x2]
-        blurred = cv2.GaussianBlur(roi, (99, 99), 0)
+        # KERNEL SIZE MUST BE AN ODD NUMBER (note: bigger number (stronger blur) = more processing)
+        blurred = cv2.GaussianBlur(roi, (131, 131), 0)
         image[y1:y2, x1:x2] = blurred
         return image
-
-    def convert_video_to_frames(self, video_name) -> list:
-        cap = cv2.VideoCapture(video_name)
-        frame_list = []
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
-            frame_list.append(frame)
-        cap.release()
-        return frame_list
 
     def apply_blur(self, frame):
         faces = self.model_detect(frame)
@@ -59,12 +53,22 @@ class ModelSetup:
             video.write(frame)
         video.release()
 
+    def convert_video_to_frames(self, video_name) -> list:
+        cap = cv2.VideoCapture(video_name)
+        frame_list = []
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame_list.append(frame)
+        cap.release()
+        return frame_list
+
+    def set_input_filename(self, input_file):
+        self.input_filename = input_file
+
     def run_steps(self, input_file):
         video_frames = self.convert_video_to_frames(input_file)
         with concurrent.futures.ThreadPoolExecutor() as executor:
             blurred_frames = list(executor.map(self.process_frame, video_frames))
         self.reassemble_blurred_frames(blurred_frames)
-
-if __name__ == "__main__":
-    model = ModelSetup()
-    model.run_steps(input_file="test2.mp4")
