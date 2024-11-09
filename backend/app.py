458350b92ede6151e09e61fff7a3ebf9c281d5fc
David Shubov <116600844+drshooby@users.noreply.github.com>
2024-11-09 11:51:21 -0800

    add: basic flask setup

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+from modelsetup import ModelSetup
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Hello World!'
+# CORS(app)
+#
+# model = ModelSetup()
+#
+# UPLOAD_FOLDER = 'uploads'
+# os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+#
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=8080)
+
+
