440e5ff4fba0ab7940331fcc43b480ae6826b6d6
Andrew <adiep@dons.usfca.edu>
2024-11-10 09:18:47 -0800

    add: veil logo

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,11 @@
 import VideoUpload from '@/components/VideoUpload';
+import Image from 'next/image';
 
 const Home = () => {
   return (
     <div className="container">
       <h1>Veil</h1>
+      <Image src="/VeilA.png" alt="Veil Logo" width={240} height={240} />
       <VideoUpload/>
     </div>
   );
